576a7801a4f18bd090828621e531d4d79f9f852a
shruthisinha <83690602+shruthisinha@users.noreply.github.com>
2021-12-22 14:55:12 +0530

    Add files via upload

---
+++ php/signup.php
@@ -0,0 +1,43 @@
+<?php  
+
+$servername ="localhost";
+$username = "root";
+$password = "";
+$dbname = "login";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if($conn->connect_error){
+	die("connection failed");
+}
+
+$name = $_POST["name"];
+$email = $_POST["email"];
+$password = $_POST["password"];
+$salt = "myfirst";
+$password_encrypted = sha1($password.$salt);
+
+
+$sql = "INSERT INTO newaccount (name, email, password) 
+VALUES ('$name', '$email', '$password_encrypted')";
+
+if($conn->query($sql) === TRUE){
+	?>
+	<script>
+		alert('Values have been inserted');
+	</script>
+	<?php
+}
+else{
+	?>
+	<script>
+		alert('Values did not insert');
+	</script>
+	<?php
+}
+echo"<script>
+window.location.href='index.html';
+</script>";
+
+
+?>
